984d4694b1f4a5a5c65b2f95bba16152bcff2a9b
Kyle Lee <hello.kylelee@gmail.com>
2017-03-02 04:16:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+var pbj = {
+  bread: 'white',
+  ingredients: ['peanut butter', 'jelly'],
+  cut: 'triangles',
+  name: 'peanut butter and jelly',
+  serve: function() {
+    console.log("here's your " + this.name + ", enjoy!");
+  }
+}
+
+function Sandwich(bread, ingredients, cut, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+  this.name = name
+  this.serve = function() {
+    console.log("Here's your " + this.name + ", enjoy!");
+  }
+}
+
+var reuben = new Sandwich('rye', ['corned beef', 'sauerkraut', 'swiss', 'russian dressing'], 'diagonal');
+
+pbj.serve();
+
+var blt = new Sandwich('white', ['bacon', 'lettuce', 'tomato', 'mayo'], 'rectangle', 'BLT');
+
+blt.serve();
