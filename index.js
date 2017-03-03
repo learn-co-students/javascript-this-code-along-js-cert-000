489b7d71330efbe64f3b63e38e2d977167a71491
Kyle Lee <hello.kylelee@gmail.com>
2017-03-03 02:13:05 +0000

    Done.

--- index.js
+++ index.js
@@ -10,20 +10,53 @@ var pbj = {
   }
 }
 
-function Sandwich(bread, ingredients, cut, name) {
+function Sandwich(bread, ingredients, name) {
   this.bread = bread;
   this.ingredients = ingredients;
-  this.cut = cut;
-  this.name = name
-  this.serve = function() {
-    console.log("Here's your " + this.name + ", enjoy!");
+  this.name = name;
+  this.describe = function() {
+    console.log('Your ' + this.name + ' includes: ' + this.ingredients.join(', ') + '. Yum!');
   }
 }
 
-var reuben = new Sandwich('rye', ['corned beef', 'sauerkraut', 'swiss', 'russian dressing'], 'diagonal');
+function deliverFood(customer, table) {
+  console.log('Delivering ' + this.name + ' to ' + customer + ' at table ' + table);
+}
 
-pbj.serve();
+function serve() {
+  if (arguments.length > 0) {
+    var customers = Array.prototype.slice.call(arguments);
+    last = customers.pop();
+    console.log(this.name + " for " + customers.join(', ') + ' and ' + last + '. Enjoy!');
+  } else {
+    console.log(this.name + ". Order up!");
+  }
+}
 
-var blt = new Sandwich('white', ['bacon', 'lettuce', 'tomato', 'mayo'], 'rectangle', 'BLT');
+var gc = new Sandwich('white', ['cheese'], 'Grilled Cheese');
+var pbj = new Sandwich('wheat', ['chunky peanut butter', 'blackberry preserves'], 'PB&Jam');
 
-blt.serve();
+pbj.describe();
+
+var salad = {
+  ingredients: ['croutons', 'romaine hearts', 'steak', 'parmesan', 'caesar dressing'],
+  name: 'Steak Caesar'
+}
+
+salad.describe = pbj.describe.bind(salad);
+
+salad.describe();
+
+function visitTable() {
+  console.log("The server is visiting " + this.name + " at table number " + this.tableNumber);
+}
+
+function Customer(name, tableNumber) {
+  this.name = name;
+  this.tableNumber = tableNumber;
+}
+
+var sally = new Customer('Sally', '4');
+
+var visitSally = visitTable.bind(sally);
+setTimeout(visitSally, 1000);
